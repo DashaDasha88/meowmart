a2a9e06f9703196c0515c81779e21d7ae9fbbb9c
Dasha <dasha.kotova.mail@gmail.com>
2021-06-21 20:19:49 -0400

    add checkout component main

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from "@material-ui/core";
 
+const steps = ['Shipping address', 'Payment details'];
+
 const Checkout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">Checkout</Typography>
+          <Stepper activeStep={0}>
+            {steps.map((step) => (
+              
+            ))}
+          </Stepper>
+        </Paper>
+      </main>
+    </>
   )
 }
 
